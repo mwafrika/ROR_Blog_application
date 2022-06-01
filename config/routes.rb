f0604ee80ea891c663e04c9dcf41102125bc0a97
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-06-01 21:54:00 +0300

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/hello'
+  get 'posts/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # root "hello#index"
-  root "pages#hello"
+  root "posts#index"
+
 end
