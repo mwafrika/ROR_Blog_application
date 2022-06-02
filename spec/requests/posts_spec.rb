38c7abe7e6693a4cebfa0dc76022d7ec49abc9d0
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-06-02 19:10:53 +0300

    Display single post for a specific user

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -19,6 +19,22 @@ RSpec.describe 'Posts Controller', type: :request do
       expect(response).to_not render_template(:show)
     end
   end
+  describe 'GET /show' do
+    it 'Redirects to the posts profile page' do
+      get '/users/id/posts/id'
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Username: Comment 10')
+    end
 
+    it 'GET users/id/posts/id should succeed' do
+      get '/users/id/posts/id'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'does not render a different template' do
+      get '/users/id/posts/id'
+      expect(response).to_not render_template(:index)
+    end
+  end
  
 end
