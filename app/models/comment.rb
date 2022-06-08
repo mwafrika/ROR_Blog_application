90f5acaecda5d83d5a79868f03c771a7f5413e0c
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-06-08 18:52:08 +0300

    Fix counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  def self.update_counter(post)
+    post.update(commentsCounter: (post.commentsCounter + 1))
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id', dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'post_id', dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  def self.update_counter(id)
+    @user = User.find(id)
+    @user.update(postsCounter: (@user.postsCounter + 1))
+  end
+
+  def recent_comments()
+    comments.last(5)
+  end
 end
