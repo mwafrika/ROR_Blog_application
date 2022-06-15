1bc5ae3282030a65727d658e3803c48450efd596
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-15 08:48:17 -0400

    Create function to destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,20 @@ class CommentsController < ApplicationController
 
   private
 
+  def destroy
+    @comment = Comment.includes(:user, :post).find(params[:id])
+    user = @comment.user
+    post = @comment.post
+
+    if @comment.destroy
+      flash[:success] = 'Comment deleted successfully'
+    else
+      flash[:error] = 'Error: Comment could not be deleted'
+    end
+
+    redirect_to user_post_path(user, post)
+  end
+
   def comment_params
     params.require(:comment).permit(:text)
   end
