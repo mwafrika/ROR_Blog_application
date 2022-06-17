cc3a11d8650780dd83175bb3214c148bd801b371
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-06-17 12:37:11 +0300

    Add more test for post

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,21 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   subject { User.new(name: 'gloire', email: 'gloire@gmail.com', password: '123456', password_confirmation: '123456') }
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'gloire', email: 'gloire@gmail.com', password: '123456', password_confirmation: '123456') }
 
-#   before { subject.save }
+  before { subject.save }
 
-#   it 'should save the data' do
-#     expect(subject).to be_valid
-#   end
+  it 'should save the data' do
+    expect(subject).to be_valid
+  end
 
-#   it 'name should be present' do
-#     subject.name = nil
-#     expect(subject).to_not be_valid
-#   end
-# end
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not use an existing email' do
+    subject.email = User.first.email
+    expect(subject).to_not be_valid
+  end
+end
