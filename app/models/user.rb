ea4944b47e6f875f57782b580a139fbeeb82ae1d
Jerry Owusu <jerryowusu50@yahoo.com>
2022-06-18 10:01:14 -0400

    Remove unused codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, class_name: 'Post', foreign_key: 'user_id', dependent: :destroy
