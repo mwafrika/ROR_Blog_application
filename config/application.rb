406b0490dc4a30745df0f9530d30e4e2e550484a
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-06-18 06:29:07 +0300

    Handle exception in the Application controller

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.api_only = true #added
 
 module BlogApp
   class Application < Rails::Application
